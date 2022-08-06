a54751699010e1c8eed9ab92160e484db12b164a
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-08-05 21:45:26 -0300

    refactor /ItemContainer

--- src/components/common/ItemContainer.tsx
+++ src/components/common/ItemContainer.tsx
@@ -10,10 +10,11 @@ const ItemContainer:React.FC<myProps> = (props: myProps)=>{
       <h2>Hottest Drops</h2>
       <h5>discover the hottest drops to upgrade your style.</h5>
       <div className='cardContainer'>
-        <CardItem/>
-        <CardItem/>
-        <CardItem/>
-        <CardItem/>
+        {
+          props.content.length>0 
+          &&
+          props.content.slice(0,4).map((sneaker:any)=><CardItem content={sneaker}/>)
+        }
       </div>
       <button>Shop Now</button>
     </div>
