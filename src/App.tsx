e164a933aa510d5e8ce4369cedaeaa84fac26dbc
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-08-09 22:13:27 -0300

    add /men and /women routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router"
 import CreateSneaker from "./components/views/CreateSneaker";
 import Home from './components/views/Home';
 import Sneaker from './components/views/Sneaker';
+import Men from './components/views/Men';
 
 function App() {
   return (
@@ -10,6 +11,9 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/new/sneaker" element={<CreateSneaker/>} />
         <Route path="/sneaker/:id" element={<Sneaker/>} />
+        <Route path="/men" element={<Men option="m"/>} />
+        <Route path="/women" element={<Men option="w"/>} />
+
       </Routes>
     </div>
   );
