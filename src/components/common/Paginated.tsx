ab1742a204fce100417d38b6765f8c1edd1dd710
Fran Mansilla <75451439+FranciscoMansilla@users.noreply.github.com>
2022-10-09 16:02:29 -0300

    feat Paginated.jsx

--- src/components/common/Paginated.tsx
+++ src/components/common/Paginated.tsx
@@ -1,18 +1,41 @@
 import CardItem from "./CardItem";
+// import Pagination from 'react-bootstrap/Pagination';
+import { useEffect } from 'react';
+import { Link } from "react-router-dom";
+
 
 interface myProps {
   content?: any;
   option?: string;
 }
+
 const Paginated:React.FC<myProps> = (props: myProps)=>{
+  const paginatedButton = (quantity: any) => {
+    const buttons = []
+    for(let i=0 ; i<quantity ; i++){
+      buttons.push(i+1)
+    }
+    return buttons
+  }
+  const numberpage = paginatedButton(props.content.numberOfPages)
+
   return(
-    <div>
+    <div className="paginated">
       <div className='cardContainer'>
         {
-          props.content.length>0 
+          props.content.sneakers.length>0 
           &&
-          props.content.map((sneaker:any)=><CardItem content={sneaker}/>)
+          props.content.sneakers.map((sneaker:any)=><CardItem content={sneaker}/>)
         }
+      </div>
+      <div className="paginatedButtonContainer">
+        {
+          numberpage.length>0 && numberpage.map(n=>
+            <Link to={`/${props.content.genre}/${n}`}>
+              <div className="paginatedButton">{n}</div>
+            </Link>
+          )
+        }      
       </div>
     </div>
   )
